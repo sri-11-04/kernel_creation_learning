6baf5785e24fb5d588f586e1397c74712070541c
sri-11-04 <srianna6778@gmail.com>
2025-12-27 21:48:18 +0530

    dec 27 9:48 pm

--- warmup_tasks/task2_my_memcpy/practice.c
+++ warmup_tasks/task2_my_memcpy/practice.c
@@ -5,8 +5,14 @@ practice for memory creation
 #include <stdio.h>
 #include <stdlib.h>
 
+void func(void *ptr){
+    printf("when moving with 1 ptr = %d\n",ptr+1);
+}
+
 int main(void){
     int *arr;
-    // arr = malloc();
+    arr = malloc(sizeof *arr);
+    func(arr);
+    free(arr);
     return 0;
 }
