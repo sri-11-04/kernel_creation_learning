3f61f6039c95182a0d4adde449dd3387819af838
sri-11-04 <srianna6778@gmail.com>
2026-02-14 10:14:28 +0530

    last modified

--- warmup_tasks/task3_rebuild_tiny_libc/main.c
+++ warmup_tasks/task3_rebuild_tiny_libc/main.c
@@ -2,11 +2,17 @@
 for any notes refer notes.txt
 */
 
-
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int main(void){
-    
+    int *arr = malloc(sizeof * arr * 10);
+    for (int i = 0; i<10;i++) printf("%d\n",*(arr+i)); 
+    printf("after mem set\n");
+    memset(arr,10,sizeof * arr);
+    for (int i = 0; i<10;i++) printf("%d\n",*(arr+i)); 
+    free(arr);
     return 0;
 }
